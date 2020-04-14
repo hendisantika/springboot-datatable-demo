07ffd5096b7ce76327a6711492119bb2cc5bc4d0
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-15 06:25:19 +0700

    #8 - Add PostConstruct OFFICES in EmployeeInitializer

--- src/main/java/com/hendisantika/springbootdatatabledemo/init/EmployeeInitializer.java
+++ src/main/java/com/hendisantika/springbootdatatabledemo/init/EmployeeInitializer.java
@@ -1,9 +1,14 @@
 package com.hendisantika.springbootdatatabledemo.init;
 
+import com.github.javafaker.Faker;
+import com.hendisantika.springbootdatatabledemo.entity.Employee;
 import com.hendisantika.springbootdatatabledemo.entity.Office;
 import com.hendisantika.springbootdatatabledemo.repository.EmployeeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.Random;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,6 +27,25 @@ class EmployeeInitializer {
 
     public EmployeeInitializer(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
+    }
+
+    @PostConstruct
+    public void init() {
+        log.info("generating {} random employees", NUMBER_TO_GENERATE);
+        Random randomGenerator = new Random();
+        Faker faker = new Faker();
+        for (int i = 1; i <= NUMBER_TO_GENERATE; i++) {
+            Employee employee = Employee.builder()
+                    .id(i)
+                    .firstName(faker.name().firstName())
+                    .lastName(faker.name().lastName())
+                    .position(faker.job().position())
+                    .age(randomGenerator.nextInt(50) + 20)
+                    .salary(randomGenerator.nextInt(20000) * 50)
+                    .office(OFFICES[randomGenerator.nextInt(OFFICES.length)])
+                    .build();
+            employeeRepository.save(employee);
+        }
     }
 
     private static final Office[] OFFICES = new Office[]{
